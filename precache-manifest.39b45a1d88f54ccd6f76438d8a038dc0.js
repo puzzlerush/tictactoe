d65fcf76f84c876cecb3d719da515280e12720ed
tacticaltofu <dingkevin@gmail.com>
2020-09-15 21:07:08 -0400

    Updates

---
+++ precache-manifest.39b45a1d88f54ccd6f76438d8a038dc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e270f50ec5b4f0307f2c32c230c3bc7",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "8037c85e2a09f8496882",
+    "url": "/tictactoe/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "01d90c3d2f3b61fef968",
+    "url": "/tictactoe/static/css/main.2782ca3b.chunk.css"
+  },
+  {
+    "revision": "8037c85e2a09f8496882",
+    "url": "/tictactoe/static/js/2.14bf8459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.14bf8459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d90c3d2f3b61fef968",
+    "url": "/tictactoe/static/js/main.9fc65eda.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  }
+]);
